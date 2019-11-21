22edc5858ee9db1941ab3f33c647f65705797dcf
adaccache <adaccache@payments.ca>
2019-11-20 20:49:12 -0500

    fetch working and some styling

--- js/utils.js
+++ js/utils.js
@@ -1,6 +1,16 @@
-export function addTextToBody(text) {
-  const div = document.createElement('div');
-  div.textContent = text;
-  document.body.appendChild(div);
-}
+  // const div = document.createElement('div');
+  // div.textContent = text;
+  // document.body.appendChild(div);
 
+
+export function callApi(text){
+  const div = document.createElement('div');
+
+fetch('https://jsonplaceholder.typicode.com/users/'+text)
+  .then(response => response.json())
+  .then(function(response){
+  	console.log(response);
+
+  })
+
+}

5e0b5a767751048965d8eb1d16dbc2c9a5a10eaa
adaccache <adaccache@payments.ca>
2019-11-20 21:51:59 -0500

    need to enable cors

--- js/utils.js
+++ js/utils.js
@@ -1,15 +1,20 @@
-  // const div = document.createElement('div');
-  // div.textContent = text;
-  // document.body.appendChild(div);
-
 
 export function callApi(text){
-  const div = document.createElement('div');
 
-fetch('https://jsonplaceholder.typicode.com/users/'+text)
-  .then(response => response.json())
-  .then(function(response){
-  	console.log(response);
+	fetch('https://api.payments.ca/fif-branch-sandbox/branches/'+text, {
+		method: 'GET', 
+		mode: 'cors',
+   		headers: {
+     	'Authorization': 'Bearer nvf8yBZT22aMm3EtYuSGAQ5dHvOc', 
+     	'Content-Type': 'application/vnd.fif.api.v1+json'
+		}
+	})
+	.headers()
+	.then(response => response.json())
+  	.then(function(response){
+
+  		console.log(response);
+  		document.getElementById("validationResults").innerHTML = '<div class="alert alert-success" role="alert">Valid</div>';
 
   })
 
